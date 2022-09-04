ff1ba4cd032a0e06d5555b1d254ca8673bc9ab0f
CodeNinja <femifadiya.segun@gmail.com>
2022-09-04 18:12:57 +0100

    scroll to the top fixed

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,17 +1,18 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Footer = () => {
   return (
     <footer>
         <div className="scrollup">
-            <a href='#mainhero'><p><i class="fa fa-angle-double-up" aria-hidden="true"></i></p></a>
+            <Link to='/'><p><i className="fa fa-angle-double-up" aria-hidden="true"></i></p></Link>
         </div>
         <div className="rightReserve">
             <p>All right Reserved. Harbode 2022</p>
         </div>
         <div className="contacts">
-            <div className="mail"><i class="fa-solid fa-envelope"></i><p>info@harbode.com</p></div>
-            <div className="ava"><i class="fa-brands fa-rocketchat"></i><p>Chat Ava Online</p></div>
+            <div className="mail"><i className="fa-solid fa-envelope"></i><p>info@harbode.com</p></div>
+            <div className="ava"><i className="fa-brands fa-rocketchat"></i><p>Chat Ava Online</p></div>
         </div>
     </footer>
   )
