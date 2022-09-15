b73d3be407ef576e042dbb15cdaaaac74f12a941
CodeNinja <femifadiya.segun@gmail.com>
2022-09-15 18:29:37 +0100

    added on the go to the page

--- src/pages/Explore.jsx
+++ src/pages/Explore.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ExploreHero from '../components/ExploreHero/ExploreHero'
 import Footer from '../components/Footer/Footer'
+import OnTheGo from '../components/OnTheGo/OnTheGo'
 const Explore = () => {
   return (
     <section className='explore'>
@@ -9,6 +10,7 @@ const Explore = () => {
         <h2>I am in between, dudes!</h2>
         <h2>I am in between, dudes!</h2>
         <h2>I am in between, dudes!</h2>
+        <OnTheGo/>
         <Footer props='/explore'/>
     </section>
   )
