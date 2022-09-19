604e7ac9d5670277dbb95cdb17f50feb3f91d7d8
CodeNinja <femifadiya.segun@gmail.com>
2022-09-19 17:40:59 +0100

    added quote sign to the testimonials

--- src/components/Testimonial/Testimonials.jsx
+++ src/components/Testimonial/Testimonials.jsx
@@ -12,7 +12,7 @@ const Testimonials = () => {
         </div>
         <div className="container">
             <div className="box">
-                <p>I can't believe there is a website that makes getting a comfortable house a seamless process. Just few scroll on the web, boom---keysssssss</p>
+                <p><i className="fa-solid fa-quote-left"></i><br/>I can't believe there is a website that makes getting a comfortable house a seamless process. Just few scroll on the web, boom---keysssssss <i className="fa-solid fa-quote-right"></i></p>
                 <div className="person">
                     <h4 className="name">Damilola Ekundayo</h4>
                     <h6><li>Product Designer</li></h6>
@@ -20,7 +20,7 @@ const Testimonials = () => {
                 <span><img src={img1} alt="" /></span>
             </div>
             <div className="box">
-                <p>I've tried a lot of platforms to advertise my available housing estate but Harbode is the best. From the way they market my houses to the way they help out with complaints.</p>
+                <p><i className="fa-solid fa-quote-left"></i><br/>I've tried a lot of platforms to advertise my available housing estate but Harbode is the best. From the way they market my houses to the way they help out with complaints. <i className="fa-solid fa-quote-right"></i></p>
                 <div className="person">
                     <h4 className="name">Helen Reuben</h4>
                     <h6><li>House Agent</li></h6>
@@ -28,7 +28,7 @@ const Testimonials = () => {
                 <span><img src={img2} id='ytb' alt="" /></span>
             </div>
             <div className="box">
-                <p>I won't say much. If you are looking for apartments, shops, co-working spaces, then Harbode should be your first and the last choice.</p>
+                <p><i className="fa-solid fa-quote-left"></i><br/>I won't say much. If you are looking for apartments, shops, co-working spaces, then Harbode should be your first and the last choice. <i className="fa-solid fa-quote-right"></i></p>
                 <div className="person">
                     <h4 className="name">Aremu Jane</h4>
                     <h6><li>Clerk</li></h6>
@@ -36,7 +36,7 @@ const Testimonials = () => {
                 <span><img src={img3} alt="" /></span>
             </div>
             <div className="box">
-                <p>Best deals happens only on Harbode. With our tight budget, their professional agents fixed us with a house that suites our need. Surely the best</p>
+                <p><i className="fa-solid fa-quote-left"></i><br/>Best deals happens only on Harbode. With our tight budget, their professional agents fixed us with a house that suites our need. Surely the best <i className="fa-solid fa-quote-right"></i></p>
                 <div className="person">
                     <h4 className="name">Mr & Mrs John</h4>
                     <h6><li>Youtuber</li></h6>
