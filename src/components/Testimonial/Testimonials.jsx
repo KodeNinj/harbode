818f1c40d3caf947e327c4411e0d02d805fde18d
CodeNinja <femifadiya.segun@gmail.com>
2022-09-19 16:24:37 +0100

    more update to the about us section

---
+++ src/components/Testimonial/Testimonials.jsx
@@ -0,0 +1,48 @@
+import React from 'react'
+
+const Testimonials = () => {
+  return (
+    <section className='testimonials'>
+        <div className="header">
+        <h2>Testimonials</h2>
+        <p>Words from our satisfied users. Our quality preceed us, and the testimonies keep following.</p>
+        </div>
+        <div className="container">
+            <div className="box">
+                <p>I can't believe there is a website that makes getting a comfortable house a seamless process. Just few scroll on the web, boom---keysssssss</p>
+                <div className="person">
+                    <h4 className="name">Damilola Ekundayo</h4>
+                    <h6><li>Product Designer</li></h6>
+                </div>
+                <span><img src='https://images.unsplash.com/photo-1503627381655-17cf0637efc0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=388&q=80' alt="" /></span>
+            </div>
+            <div className="box">
+                <p>I can't believe there is a website that makes getting a comfortable house a seamless process. Just few scroll on the web, boom---keysssssss</p>
+                <div className="person">
+                    <h4 className="name">Damilola Ekundayo</h4>
+                    <h6><li>Product Designer</li></h6>
+                </div>
+                <span><img src='https://images.unsplash.com/photo-1503627381655-17cf0637efc0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=388&q=80' alt="" /></span>
+            </div>
+            <div className="box">
+                <p>I can't believe there is a website that makes getting a comfortable house a seamless process. Just few scroll on the web, boom---keysssssss</p>
+                <div className="person">
+                    <h4 className="name">Damilola Ekundayo</h4>
+                    <h6><li>Product Designer</li></h6>
+                </div>
+                <span><img src='https://images.unsplash.com/photo-1503627381655-17cf0637efc0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=388&q=80' alt="" /></span>
+            </div>
+            <div className="box">
+                <p>I can't believe there is a website that makes getting a comfortable house a seamless process. Just few scroll on the web, boom---keysssssss</p>
+                <div className="person">
+                    <h4 className="name">Damilola Ekundayo</h4>
+                    <h6><li>Product Designer</li></h6>
+                </div>
+                <span><img src='https://images.unsplash.com/photo-1503627381655-17cf0637efc0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=388&q=80' alt="" /></span>
+            </div>
+        </div>
+    </section>
+  )
+}
+
+export default Testimonials
--- src/pages/AboutUs.jsx
+++ src/pages/AboutUs.jsx
@@ -5,6 +5,7 @@ import MissionVision from '../components/MissionAndVission/MissionVision'
 import Navbar from '../components/Navigation/Navbar'
 import OnTheGo from '../components/OnTheGo/OnTheGo'
 import Partners from '../components/Partners/Partners'
+import Testimonials from '../components/Testimonial/Testimonials'
 import AboutImg from '../img/aboutus.png'
 import AboutImg2 from '../img/aboutus1.png'
 const AboutUs = (props) => {
@@ -13,8 +14,8 @@ const AboutUs = (props) => {
       <Navbar/>
       <AboutHero/>
       <MissionVision/>
-      
       <Partners/>
+      <Testimonials/>
       <OnTheGo/>
       <Footer/>
     </section>
