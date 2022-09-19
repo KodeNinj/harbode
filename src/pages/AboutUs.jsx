b12677684eb13e124c3da2abf5e02cb32b203050
CodeNinja <femifadiya.segun@gmail.com>
2022-09-19 14:55:47 +0100

    added sections to the about us page

--- src/pages/AboutUs.jsx
+++ src/pages/AboutUs.jsx
@@ -1,14 +1,20 @@
 import React from 'react'
 import AboutHero from '../components/AboutUsHero/AboutHero'
 import Footer from '../components/Footer/Footer'
+import MissionVision from '../components/MissionAndVission/MissionVision'
 import Navbar from '../components/Navigation/Navbar'
 import OnTheGo from '../components/OnTheGo/OnTheGo'
-
-const AboutUs = () => {
+import Partners from '../components/Partners/Partners'
+import AboutImg from '../img/aboutus.png'
+import AboutImg2 from '../img/aboutus1.png'
+const AboutUs = (props) => {
   return (
     <section className='about_Us'>
       <Navbar/>
       <AboutHero/>
+      <MissionVision/>
+      
+      <Partners/>
       <OnTheGo/>
       <Footer/>
     </section>
